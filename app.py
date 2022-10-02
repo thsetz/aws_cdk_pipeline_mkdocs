925f43e1985de17a06276c2742a8d301e316a2fb
Thomas Setz <thomas@setz.de>
2022-10-02 21:50:59 +0200

    xx

--- app.py
+++ app.py
@@ -3,14 +3,14 @@
 
 from os import environ
 import aws_cdk as cdk
-#from aws_cdk_pipeline.aws_cdk_pipeline_stack import AwsCdkPipelineStack
+from aws_cdk_pipeline.aws_cdk_pipeline_stack import AwsCdkPipelineStack
 from aws_cdk_pipeline.pipeline_stack import PipelineStack
 
 APP_ACCOUNT= environ.get("APP_ACCOUNT")
 AWS_REGION = environ.get("AWS_REGION")
 
 app = cdk.App()
-#AwsCdkPipelineStack( app, "AwsCdkPipelineStack", env={ })
+AwsCdkPipelineStack( app, "AwsCdkPipelineStack", env={ })
 
 PipelineStack(app, "AwsCdkPipeline", env={ 'account': APP_ACCOUNT, 'region': AWS_REGION })
 
